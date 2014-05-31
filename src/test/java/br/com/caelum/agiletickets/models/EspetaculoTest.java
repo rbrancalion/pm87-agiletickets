94ec6cc4e456b3b8be6f792321fbf42ffa3bd6a5
roberto <roberto.brancalion@credsystem.com.br>
2014-05-31 15:17:26 -0300

    ok2

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -97,7 +97,7 @@ public class EspetaculoTest {
 		DateTime dataFim = dataInicio.plusDays(3);
 		
 		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio.toLocalDate(), dataFim.toLocalDate(), dataInicio.toLocalTime(), Periodicidade.DIARIA);
-		int dias = Days.daysBetween(dataInicio, dataFim).getDays();
+		int dias = Days.daysBetween(dataInicio, dataFim).getDays()+1;
 		
 		Assert.assertEquals(dias, sessoes.size());
 		
